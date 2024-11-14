bc336028818ccacb77f9f732389862d7d7c32075
Debajyati Dey <ddebajyati@gmail.com>
2024-11-15 02:23:31 +0530

    object for handling unauthentication and authentication error related issues

---
+++ src/auth/unauthentication.js
@@ -0,0 +1,18 @@
+/*
+ * for handling unauthentication and authentication error related issues 
+ * */
+
+export default {
+  type: "unauthenticated",
+  reason: (errorCode) => {
+    if (errorCode === "access-denied") {
+      return "User has denied the request. The authorization process has been canceled.";
+    } else if (errorCode === "incorrect_device_code") {
+      return "The device code provided is not valid.";
+    } else if (errorCode === "expired_token") {
+      return "The device code has expired. Please start the process again.";
+    } else {
+      return "The app's access has been revoked by GitHub!\n\t Or Unknown Error : (";
+    }
+  }
+}
