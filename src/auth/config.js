09f880659be24261ac036b5e6185f3c0276609ed
Debajyati Dey <ddebajyati@gmail.com>
2024-11-15 02:26:11 +0530

    config object that contains client id and config file path

---
+++ src/auth/config.js
@@ -0,0 +1,9 @@
+import path from "node:path";
+import os from "os";
+
+const config = {
+  CLIENT_ID: "Ov23li5gcNYDdMUWPeCt",
+  TOKEN_FILE: path.join(os.homedir(), '.gitfmrc.json'),
+}
+
+export default config;
