7d22132d3b4a3dcf710db65b0f15f7a0f7312de6
Debajyati Dey <ddebajyati@gmail.com>
2024-11-17 02:12:58 +0530

    gitlab auth commands

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Command } from 'commander';
 import { clearToken, getStoredAuthType, getStoredToken } from './src/gh/auth/tokenHelpers.js';
 import config from "./src/gh/auth/config.js";
 import { userProfile, login } from "./src/gh/utils/authenticated/requests.js";
+import { CONFIG_FILE as GITLAB_CONFIG_FILE } from './src/gl/requests.js';
 
 const packageJson = await (async ()=>{
   const { createRequire } = await import("node:module");
@@ -62,25 +63,63 @@ program
   });
 
 program
-  .command('profile')
-  .description("fetch a minimal overview of your GitHub profile")
-  .action(async () => {
-    const octokit = await login();
-    await userProfile(octokit);
-  });
-
-program
   .command('glauth')
   .description("authorize or unauthorize gitfm with your GitLab")
   .option('--login', "login with a personal access token")
   .option('--logout', "logout and revoke the token")
+  .option('--rotate <EXPIRY_DATE>', "rotate the personal access token with an Expiry Date (NOTE: Expiry Date has to be in YYYY-MM-DD format!)")
   .action(async (options) => {
-    if (options.logout) {
-      // TODO:
-    } else {
-      const { tokenAuthenticate } = await import("./src/gl/auth.js");
-      return await tokenAuthenticate();
+    try {
+      if (options.logout) {
+        const { revokeToken, configJson } = await import("./src/gl/requests.js");
+        const token = configJson.token;
+        if (token !== null) {
+          const resStatus = await revokeToken(token);
+          if (resStatus === "successful") {
+            console.log("Token successfully revoked!");
+          } else if (resStatus === "unsuccessful") {
+            console.error("Bad Request: Couldn't revoke successfully!");
+            process.exit(1);
+          } else if (resStatus === "invalid") {
+            console.error("The access token is invalid!");
+            process.exit(1);
+          }
+        } else {
+          console.error("Can't Logout: Not currently Authenticated!");
+          process.exit(1);
+        }
+      } else if (options.login) {
+        const { tokenAuthenticate } = await import("./src/gl/auth.js");
+        const token = await tokenAuthenticate();
+        const { saveToken } = await import("./src/gl/tokenhelpers.js");
+        saveToken({token: token}, GITLAB_CONFIG_FILE);
+      } else {
+        const { rotateToken, configJson } = await import("./src/gl/requests.js");
+        const token = configJson.token;
+        const { saveToken } = await import("./src/gl/tokenhelpers.js");
+        const expiryDate = options.rotate;
+
+        const newToken = await rotateToken(token, expiryDate);
+        saveToken({token: newToken}, GITLAB_CONFIG_FILE);
+      }
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
     }
-  })
+  });
+
+program
+  .command('profile')
+  .description("get a minimal overview of your profile")
+  .option('--gl', "GitLab profile")
+  .option('--gh', "GitHub profile")
+  .action(async (options) => {
+    if (options.gh) {
+      const octokit = await login();
+      await userProfile(octokit);
+    } else {
+      // TODO:
+    }
+  });
 
 program.parse();
