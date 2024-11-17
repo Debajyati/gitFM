dbfe62669e0fa1b382005f92913fc57b9d17ddaf
Debajyati Dey <ddebajyati@gmail.com>
2024-11-18 01:25:21 +0530

    remove axios from dependencies

--- src/gl/auth.js
+++ src/gl/auth.js
@@ -2,11 +2,7 @@ import input from '../gh/utils/input.js';
 
 const baseURL = "https://gitlab.com/api/v4";
 
-const tokenAuthenticate = async () => {
-  console.log('Create a new personal access token from your GitLab profile');
-  console.log('Choose both \'read_api\' and \'read-user\' scopes from the checkboxes.');
-  const token = await input('Enter the token here');
-
+const checkTokenIsValid = async (token) => {
   const url = `${baseURL}/personal_access_tokens`;
 
   try {
@@ -17,10 +13,8 @@ const tokenAuthenticate = async () => {
       }
     });
 
-    const data = await response.json();
-
-    if (data.message === "401 Unauthorized") {
-      console.error("invalid access token: Authentication aborted!");
+    if (response.status === 401) {
+      console.error("invalid access token: Aborted!");
       process.exit(1);
     } else {
       console.log("Authentication Successful");
@@ -30,6 +24,13 @@ const tokenAuthenticate = async () => {
     console.error(error.message);
     process.exit(1);
   }
+}
+
+const tokenAuthenticate = async () => {
+  console.log('Create a new personal access token from your GitLab profile');
+  console.log('Choose only the \'api\' scope from the checkboxes.');
+  const token = await input('Enter the token here');
+  return await checkTokenIsValid(token);
 }
 
 export { tokenAuthenticate };
