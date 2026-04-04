2a1b001aef81e9ce9ae041b3d890332e880da893
Debajyati Dey <ddebajyati@gmail.com>
2026-04-04 21:00:19 +0530

    time to migrate from js to ts

--- src/gh/auth/config.js
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import os from "os";
-
-const config = {
-  CLIENT_ID: "Ov23liEZlvbyKNsSPB1n",
-  TOKEN_FILE: path.join(os.homedir(), '.gitfmrc.json'),
-}
-
-export default config;
--- src/gl/config.js
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-import os from "os";
-
-const config = {
-  TOKEN_FILE: path.join(os.homedir(), '.gl.gitfmrc.json'),
-};
-
-export { config };
